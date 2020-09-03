dbc00d2400ffcf7b239f78d3a33d386897d6f6ea
veerakumarasamy <veerakumarasamy@users.noreply.github.com>
2020-09-03 19:00:24 +0530

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,60 @@
+package com.alation.infor.dih;
+
+public class Constants {
+    public static final String SOURCE = "SOURCE";
+    public static final String SOURCEFIELD = "SOURCEFIELD";
+    public static final String NAME = "NAME";
+    public static final String BUSINESS_NAME = "BUSINESSNAME";
+    public static final String TOPIC_NAME = "TOPIC_NAME";
+    public static final String CONNECTOR = "CONNECTOR";
+    public static final String TRANSFORMATION = "TRANSFORMATION";
+    public static final String TRANSFORM_FIELD = "TRANSFORMFIELD";
+    public static final String TABLE_ATTRIBUTE = "TABLEATTRIBUTE";
+    public static final String PORT_TYPE = "PORTTYPE";
+    public static final String FILTER_CONDITION = "Filter Condition";
+    public static final String LOOKUP_SQL_QUERY = "Lookup Sql Query";
+    public static final String OUTPUT = "OUTPUT";
+    public static final String EXPRESSION = "EXPRESSION";
+    public static final String FROM_INSTANCE = "FROMINSTANCE";
+    public static final String FROM_INSTANCE_TYPE = "FROMINSTANCETYPE";
+    public static final String FROM_FIELD = "FROMFIELD";
+    public static final String TO_FIELD = "TOFIELD";
+    public static final String TO_INSTANCE = "TOINSTANCE";
+    public static final String TO_INSTANCE_TYPE = "TOINSTANCETYPE";
+    public static final String TARGET_DEFINITION = "Target Definition";
+    public static final String TARGET = "TARGET";
+    public static final String DATABASE_TYPE = "DATABASETYPE";
+    public static final String FLAT_FILE = "FLAT FILE";
+    public static final String DIH = "[DIH] ";
+    public static final String LOG = "log";
+    public static final String TEXT = ".log";
+    public static final String DATE_FORMAT = "yyyy-MM-dd_s_S";
+    public static final String SESSION = "SESSION";
+    public static final String MAPPING = "MAPPING";
+    public static final String MAPPINGNAME = "MAPPINGNAME";
+    public static final String SOURCE_QUALIFIER = "Source Qualifier";
+    public static final String TARGET_FIELD = "TARGETFIELD";
+    public static final String START = "START";
+    public static final String COMMAND = "COMMAND";
+    public static final String WORKLET = "WORKLET";
+    public static final String TRANSFORMATION_NAME = "TRANSFORMATION_NAME";
+    public static final String COLUMN = "column";
+    public static final String TABLE = "table";
+    public static final String SQL_QUERY = "Sql Query";
+    public static final String LOOKUP_SQL_OVERRIDE = "Lookup Sql Override";
+
+
+    public static final String SRC_SCHEMA = "SRC_SCHEMA";
+    public static final String TGT_SCHEMA = "TGT_SCHEMA";
+    public static final String SRC_TABLENAME = "SRC_TABLENAME";
+    public static final String TGT_TABLENAME = "TGT_TABLENAME";
+    public static final String TMP = "TMP";
+	public static final String FILTER_PREFFIX = "filter/";
+	public static final String PAYLOAD = "payload";
+	public static final String WORKFLOW = "WORKFLOW";
+	public static final String TASKINSTANCE = "TASKINSTANCE";
+	public static final String TASKTYPE = "TASKTYPE";
+	public static final String TASKNAME = "TASKNAME";
+    public static final String ALATION_OVERALL_DATASOURCES_TABLES = "AlationOverAllDatasourcesTables";
+
+}
